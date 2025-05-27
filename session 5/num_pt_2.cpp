54df24cd9d80d42464e8145082292b80694a23ab
AIndrajith <deegalla12378@usci.ruh.ac.lk>
2025-05-27 22:16:16 +0530

    star patterns and number patters

---
+++ session 5/num_pt_2.cpp
@@ -0,0 +1,10 @@
+// multiplication table of 5
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    for(int i = 1; i<=10; i++){
+        cout<< "5 x "<< i<< " = " << i*5 << endl; 
+    }
+}
